2673ad534ed2aa052758150b1bbc3b6569b7f30a
fr3d100 <f.bonnand@gmail.com>
2019-01-31 08:05:45 +0100

    seed completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# On détruit les données en base
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+
+
+# On créé 5 docteurs
+
+5.times do 
+	Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: Faker::Job.title )
+end
+puts "5 docteurs ont été créés"
+
+# On créé 10 patients
+10.times do 
+	Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+puts "10 patients ont été créés"
+
+
+# On créé 50 RDV 
+50.times do 
+	Appointment.create(doctor: Doctor.order("RANDOM()").first ,patient: Patient.order("RANDOM()").first)
+end
+puts "50 RDV ont été créés"
