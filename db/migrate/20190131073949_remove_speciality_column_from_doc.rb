fa1e430a0138a74f0bb67f31dc8743791a7dae4c
fr3d100 <f.bonnand@gmail.com>
2019-01-31 08:48:54 +0100

    Speciality model ok

---
+++ db/migrate/20190131073949_remove_speciality_column_from_doc.rb
@@ -0,0 +1,5 @@
+class RemoveSpecialityColumnFromDoc < ActiveRecord::Migration[5.2]
+  def change
+  	remove_column :doctors, :speciality
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_070755) do
+ActiveRecord::Schema.define(version: 2019_01_31_074158) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "doctor_id"
@@ -32,12 +32,20 @@ ActiveRecord::Schema.define(version: 2019_01_31_070755) do
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "speciality"
     t.integer "postalcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "city_id"
     t.index ["city_id"], name: "index_doctors_on_city_id"
+  end
+
+  create_table "join_table_speciality_doctors", force: :cascade do |t|
+    t.integer "doctor_id"
+    t.integer "speciality_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_join_table_speciality_doctors_on_doctor_id"
+    t.index ["speciality_id"], name: "index_join_table_speciality_doctors_on_speciality_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -47,6 +55,12 @@ ActiveRecord::Schema.define(version: 2019_01_31_070755) do
     t.datetime "updated_at", null: false
     t.integer "city_id"
     t.index ["city_id"], name: "index_patients_on_city_id"
+  end
+
+  create_table "specialities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
